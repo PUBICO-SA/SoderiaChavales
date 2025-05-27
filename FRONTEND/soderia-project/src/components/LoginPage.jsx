2a9d3e7cb5cb799c4f279c615f064d862ecd9254
tommy <tomasjerezjavier3@gmail.com>
2025-05-26 22:44:15 -0300

    estilitos2

--- FRONTEND/soderia-project/src/components/LoginPage.jsx
+++ FRONTEND/soderia-project/src/components/LoginPage.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../stores/authStore";
-import {ENDPOINT_USERS, ENDPOINT_CLIENTES} from "../routes/routes"; // Adjust the import path as necessary
+import { ENDPOINT_USERS, ENDPOINT_CLIENTES } from "../routes/routes";
 import axios from "axios";
+import "../styles/LoginPage.css";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
@@ -51,9 +52,9 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
+    <div className="login-container">
       <h2>Iniciar sesión</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {error && <p>{error}</p>}
       <form onSubmit={handleLogin}>
         <input
           type="text"
@@ -62,7 +63,6 @@ const LoginPage = () => {
           onChange={(e) => setUsername(e.target.value)}
           required
         />
-        <br />
         <input
           type="password"
           placeholder="Contraseña"
@@ -70,7 +70,6 @@ const LoginPage = () => {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-        <br />
         <button type="submit">Entrar</button>
       </form>
     </div>
